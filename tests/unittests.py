3571ad6ca971a7e9e0a58b5a5cd0d8c4648a0d16
apchsh <apc34@le.ac.uk>
2018-04-29 21:53:59 +0100

    Modified the unit tests

--- tests/unittests.py
+++ tests/unittests.py
@@ -1,15 +1,25 @@
 import unittest
 import sys; sys.path.append("..")
 
-from params import Options, KeyValueError, KeyMissingError
+from validate import Validator, KeyValueError, KeyMissingError, KeyNotKnownError
 
 class TestParams(unittest.TestCase): 
 
-    def test_par(self):
-
+    def test_par_few(self):
+        '''Test to check that the correct exception is raised if too few 
+        keyword parameters are passed to the Validator class.'''
         d = {"HELP":"ME"}
         with self.assertRaises(KeyMissingError):
-            Options(d)
+            Validator(d)
+
+    def test_par_many(self):
+        '''Test to check what happens when a large number of header keywords are passed which don't match the list in the Validator class.'''
+
+        d = {} 
+        for count in range(0, 1000): d[str(count)] = count 
+
+        with self.assertRaises(KeyNotKnownError):
+            Validator(d)
 
 
 if __name__ == "__main__":
